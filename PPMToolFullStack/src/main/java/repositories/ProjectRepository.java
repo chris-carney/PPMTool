8a26592cfa6ca51610b225d24323c5b1d00da84d
Chris Carney <ccarney95@gmail.com>
2020-05-30 15:26:13 -0400

    Project Repository

--- PPMToolFullStack/src/main/java/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/repositories/ProjectRepository.java
@@ -1,4 +1,13 @@
 package repositories;
 
-public interface ProjectRepository {
+import domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Id;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
 }
