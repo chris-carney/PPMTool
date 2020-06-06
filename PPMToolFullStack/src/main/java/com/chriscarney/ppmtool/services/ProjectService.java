36bb465bfd36f21ce123952bcf6b37cd45ab0d75
Chris Carney <ccarney95@gmail.com>
2020-06-06 14:13:33 -0400

    Validation and Exceptions

--- PPMToolFullStack/src/main/java/com/chriscarney/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/chriscarney/ppmtool/services/ProjectService.java
@@ -2,6 +2,7 @@ package com.chriscarney.ppmtool.services;
 
 import com.chriscarney.ppmtool.domain.Project;
 import com.chriscarney.ppmtool.repositories.ProjectRepository;
+import exceptions.ProjectIdException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,12 @@ public class ProjectService{
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch(Exception e){
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
+        }
     }
 }
