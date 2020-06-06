17dc44ce968a69ba618a732116f65e6837d9fdc3
Chris Carney <ccarney95@gmail.com>
2020-06-06 15:10:54 -0400

    (Read) Finding Projects

--- PPMToolFullStack/src/main/java/com/chriscarney/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/chriscarney/ppmtool/services/ProjectService.java
@@ -12,12 +12,27 @@ public class ProjectService{
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project){
-        try {
+        try
+        {
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
             return projectRepository.save(project);
         }
         catch(Exception e){
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
         }
+    }
+
+    public Project findProjectByIdentifier(String projectId){
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if(project == null){
+            throw new ProjectIdException("Project ID '" + projectId + "' does not exists.");
+        }
+
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
     }
 }
