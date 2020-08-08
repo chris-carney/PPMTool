3199d138fa76c2a535a858a6cf7e5568d5877a51
Chris Carney <ccarney95@gmail.com>
2020-08-08 12:06:07 -0400

    Added React template

--- PPMToolFullStack/src/main/java/com/chriscarney/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/chriscarney/ppmtool/services/ProjectService.java
@@ -11,19 +11,19 @@ public class ProjectService{
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdateProject(Project project){
-        try
-        {
-            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
-            return projectRepository.save(project);
+        public Project saveOrUpdateProject(Project project){
+            try
+            {
+                project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+                return projectRepository.save(project);
+            }
+            catch(Exception e){
+                throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
+            }
         }
-        catch(Exception e){
-            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists.");
-        }
-    }
 
-    public Project findProjectByIdentifier(String projectId){
-        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        public Project findProjectByIdentifier(String projectId){
+            Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
 
         if(project == null){
             throw new ProjectIdException("Project ID '" + projectId + "' does not exists.");
